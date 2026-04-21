6ccffd9a933113ae1a36135fb8db746200b4660b
uncommonde4th <uncommonde4th@gmail.com>
2026-04-21 23:19:35 +0300

    AVL 100%

--- TreeDataStructures/Implementations/AVL/AvlTree.cs
+++ TreeDataStructures/Implementations/AVL/AvlTree.cs
@@ -10,8 +10,48 @@ public class AvlTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, AvlNode<
     
     protected override void OnNodeAdded(AvlNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Rebalance(newNode);
     }
 
-    
+    protected override void OnNodeRemoved(AvlNode<TKey, TValue>? parent, AvlNode<TKey, TValue>? child)
+    {
+        Rebalance(parent ?? child);
+    }
+
+    private static int GetHeight(AvlNode<TKey, TValue>? node) => node?.Height ?? 0;
+
+    private static int GetBalance(AvlNode<TKey, TValue>? node) => node == null ? 0 : GetHeight(node.Left) - GetHeight(node.Right);
+
+    private static void UpdateHeight(AvlNode<TKey, TValue>? node)
+    {
+        if (node != null) { node.Height = 1 + Math.Max(GetHeight(node.Left), GetHeight(node.Right)); }
+    }
+
+    private void Rebalance(AvlNode<TKey, TValue>? node)
+    {
+        while (node != null)
+        {
+            UpdateHeight(node);
+            int balance = GetBalance(node);
+
+            if (balance > 1)
+            {
+                if (GetBalance(node.Left) < 0) { RotateBigRight(node) ;}
+                else { RotateRight(node); }
+
+                UpdateHeight(node);
+                UpdateHeight(node.Parent);
+            }
+            else if (balance < -1)
+            {
+                if (GetBalance(node.Right) > 0) { RotateBigLeft(node); }
+                else { RotateLeft(node); }
+
+                UpdateHeight(node);
+                UpdateHeight(node.Parent);
+            }
+
+            node = node.Parent;
+        }
+    }  
 }
